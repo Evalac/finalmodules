13f7de380cc9405e4350d113ae65b2832b6adac4
Evalac <maslenokprograming@gmail.com>
2024-07-19 14:44:20 +0300

    1111

---
+++ src/js/01-timeout.js
@@ -0,0 +1,14 @@
+const logMessage = () => {
+  console.log('Лог при виклику callback-функціїї через 3 секунди');
+};
+
+console.log('до виклику setTimeout');
+
+setTimeout(() => {
+  console.log('в середині функціїї setTimeout');
+}, 2000);
+
+console.log('після виклику setTimeout');
+for (let i = 0; i < 100000; i++) {
+  console.log(i);
+}
