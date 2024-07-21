4148a404c3a4195f18eea9023f3a286f8ea3ee9a
Evalac <maslenokprograming@gmail.com>
2024-07-21 15:03:24 +0300

    1111

--- src/js/03-notification.js
+++ src/js/03-notification.js
@@ -2,6 +2,9 @@ const refs = {
   notification: document.querySelector('.js-alert'),
 };
 
+const NOTIFICATION_DELAY = 3000;
+let timeOutID = null;
+
 refs.notification.addEventListener('click', onNotificationClick);
 
 console.log(refs.notification);
@@ -9,12 +12,17 @@ showNotification();
 
 function onNotificationClick(evt) {
   hiddeNotification();
+  clearInterval(timeOutID);
 }
 
 function showNotification() {
-  refs.notification.classList.add('.is-visible');
+  refs.notification.classList.add('is-visible');
+  timeOutID = setTimeout(() => {
+    console.log('выдкладенне закриття');
+    hiddeNotification();
+  }, NOTIFICATION_DELAY);
 }
 
 function hiddeNotification() {
-  refs.notification.classList.remove('.is-visible');
+  refs.notification.classList.remove('is-visible');
 }
