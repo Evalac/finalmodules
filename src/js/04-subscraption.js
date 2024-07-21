b92b5c52075ee658864cb1144002e7f59af26a68
Evalac <maslenokprograming@gmail.com>
2024-07-21 19:08:44 +0300

    1111

--- src/js/04-subscraption.js
+++ src/js/04-subscraption.js
@@ -1,12 +1,42 @@
 import { Modal } from 'bootstrap.native';
 const modal = new Modal('#staticBackdrop');
-modal.show();
-// {
-//   const PROMPT_DALLY = 1000;
-//   const MAX_PROMPT_ATTEMPS = 3;
 
-//   let pormptCounetr = 0;
-//   let hasSubscripe = false;
+const PROMPT_DALLY = 1000;
+const MAX_PROMPT_ATTEMPS = 3;
+let pormptCounetr = 0;
+let hasSubscripe = false;
+
+const refs = {
+  modal: document.querySelector('#staticBackdrop'),
+  subscribeBtn: document.querySelector('button[data-subscraption]'),
+};
+console.log(refs.subscribeBtn);
+
+openSpamModal();
+
+refs.modal.addEventListener('hide.bs.modal', openSpamModal);
+refs.subscribeBtn.addEventListener('click', onSubBtnClick);
+
+function openSpamModal() {
+  if (pormptCounetr === MAX_PROMPT_ATTEMPS || hasSubscripe) {
+    console.log('Max click close or subscripe');
+    return;
+  }
+  setTimeout(() => {
+    console.log('Open modal');
+    modal.show();
+    pormptCounetr += 1;
+  }, PROMPT_DALLY);
+}
+
+function onSubBtnClick() {
+  hasSubscripe = true;
+  modal.hide();
+}
+
+// {
+
+//
 
 //   const inervalId = setInterval(() => {
 //     if (pormptCounetr === MAX_PROMPT_ATTEMPS || hasSubscripe) {
