2752f4776452e0d69898424044129136496e4d00
Evalac <maslenokprograming@gmail.com>
2024-07-21 14:18:30 +0300

    1111

--- src/js/03-notification.js
+++ src/js/03-notification.js
@@ -1,3 +1,20 @@
 const refs = {
   notification: document.querySelector('.js-alert'),
 };
+
+refs.notification.addEventListener('click', onNotificationClick);
+
+console.log(refs.notification);
+showNotification();
+
+function onNotificationClick(evt) {
+  hiddeNotification();
+}
+
+function showNotification() {
+  refs.notification.classList.add('.is-visible');
+}
+
+function hiddeNotification() {
+  refs.notification.classList.remove('.is-visible');
+}
