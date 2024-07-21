cbf0636b00427c60df48387b18b334bf972f95a5
Evalac <maslenokprograming@gmail.com>
2024-07-21 17:46:57 +0300

    1111

--- src/js/04-subscraption.js
+++ src/js/04-subscraption.js
@@ -1,15 +1,18 @@
-const PROMPT_DALLY = 1000;
-const MAX_PROMPT_ATTEMPS = 3;
+import BSN from 'bootstrap.native';
+// {
+//   const PROMPT_DALLY = 1000;
+//   const MAX_PROMPT_ATTEMPS = 3;
 
-let pormptCounetr = 0;
-let hasSubscripe = false;
+//   let pormptCounetr = 0;
+//   let hasSubscripe = false;
 
-const inervalId = setInterval(() => {
-  if (pormptCounetr === MAX_PROMPT_ATTEMPS || hasSubscripe) {
-    clearInterval(inervalId);
-    return;
-  }
-  console.log(`Subscripe Now - ` + Date.now());
-  pormptCounetr += 1;
-  console.log(pormptCounetr);
-}, PROMPT_DALLY);
+//   const inervalId = setInterval(() => {
+//     if (pormptCounetr === MAX_PROMPT_ATTEMPS || hasSubscripe) {
+//       clearInterval(inervalId);
+//       return;
+//     }
+//     console.log(`Subscripe Now - ` + Date.now());
+//     pormptCounetr += 1;
+//     console.log(pormptCounetr);
+//   }, PROMPT_DALLY);
+// }
