bf7fd2db77d67e065efa1c542867c7dad8ffcd44
Evalac <maslenokprograming@gmail.com>
2024-07-21 15:31:20 +0300

    1111

---
+++ src/js/04-subscraption.js
@@ -0,0 +1,15 @@
+const PROMPT_DALLY = 1000;
+const MAX_PROMPT_ATTEMPS = 3;
+
+let pormptCounetr = 0;
+let hasSubscripe = false;
+
+const inervalId = setInterval(() => {
+  if (pormptCounetr === MAX_PROMPT_ATTEMPS || hasSubscripe) {
+    clearInterval(inervalId);
+    return;
+  }
+  console.log(`Subscripe Now - ` + Date.now());
+  pormptCounetr += 1;
+  console.log(pormptCounetr);
+}, PROMPT_DALLY);
