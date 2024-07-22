4227767cb1cfb798338fe3a4eba4beeea19bc7c8
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-22 15:06:14 +0300

    timer

--- src/js/06-timer.js
+++ src/js/06-timer.js
@@ -1,7 +1,21 @@
 const timer = {
   start() {
-    console.log('start');
+    const startTime = Date.now();
+    setInterval(() => {
+      const currentTime = Date.now();
+      const timeDifference = currentTime - startTime;
+      const timeToFunction = getTimeComponents(timeDifference);
+      console.log(timeToFunction);
+    }, 1000);
   },
 };
 
 timer.start();
+
+function getTimeComponents(time) {
+  const days = Math.floor(time / 1000 / 60 / 60 / 24);
+  const hours = Math.floor(time / 1000 / 60 / 60) % 24;
+  const minutes = Math.floor(time / 1000 / 60) % 60;
+  const seconds = Math.floor(time / 1000) % 60;
+  return { days, hours, minutes, seconds };
+}
