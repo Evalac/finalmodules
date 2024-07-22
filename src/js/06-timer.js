c0bbaeec31b56277dc31f81536347082cfcfe727
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-22 16:09:31 +0300

    timer

--- src/js/06-timer.js
+++ src/js/06-timer.js
@@ -1,19 +1,42 @@
+const refs = {
+  startBtn: document.querySelector('button[data-action-start]'),
+  stopBtn: document.querySelector('button[data-action-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartTimer);
+refs.stopBtn.addEventListener('click', onStopTimer);
+
+function onStartTimer() {
+  timer.start();
+}
+function onStopTimer() {
+  timer.stop();
+}
+
 const timer = {
+  inervalID: null,
+  isActive: false,
   start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
     const startTime = Date.now();
-    setInterval(() => {
+    this.inervalID = setInterval(() => {
       const currentTime = Date.now();
       const timeDifference = currentTime - startTime;
-      const { days, hours, minutes, seconds } =
-        getTimeComponents(timeDifference);
-      console.log(`${days}:${hours}:${minutes}:${seconds}`);
+      const time = getTimeComponents(timeDifference);
+      updateClockFace(time);
     }, 1000);
+  },
+  stop() {
+    clearInterval(this.inervalID);
+    this.isActive = false;
   },
 };
 
-timer.start();
-
 function pad(value) {
+  // функція яка додає 0 перед першим числом якшо вже два числа 0 не додається
   return String(value).padStart(2, '0');
 }
 
@@ -24,3 +47,5 @@ function getTimeComponents(time) {
   const seconds = pad(Math.floor(time / 1000) % 60);
   return { days, hours, minutes, seconds };
 }
+
+function updateClockFace({ days, hours, minutes, seconds }) {}
