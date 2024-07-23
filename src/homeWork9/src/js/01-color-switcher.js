1bd77f7cc2291698dd75bd01b426cabade29e215
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-23 16:42:10 +0300

    1111

--- src/homeWork9/src/js/01-color-switcher.js
+++ src/homeWork9/src/js/01-color-switcher.js
@@ -2,7 +2,6 @@ const refs = {
   startBtn: document.querySelector('button[data-start]'),
   stopBtn: document.querySelector('button[data-stop]'),
 };
-let setIntervalId = null;
 
 class ChangeColor {
   constructor() {
