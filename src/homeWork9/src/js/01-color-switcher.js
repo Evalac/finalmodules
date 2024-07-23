aac744377e9784de99b89fbaad115c0b450c1655
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-23 15:42:47 +0300

    1111

--- src/homeWork9/src/js/01-color-switcher.js
+++ src/homeWork9/src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', () => {
+  console.log('satrt');
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  console.log('stop');
+});
