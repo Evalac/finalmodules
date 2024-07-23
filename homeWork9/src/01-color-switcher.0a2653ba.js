2985ef0ae8a80ff0cb4e027917d793603ba7f71e
Evalac <102866941+Evalac@users.noreply.github.com>
2024-07-23 13:34:01 +0000

    Deploying to gh-pages from @ Evalac/finalmodules@a41b184e9a19124c94db2f79295cc9091600c7a4 🚀

--- homeWork9/src/01-color-switcher.0a2653ba.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(()=>{console.log("satrt")})),t.stopBtn.addEventListener("click",(()=>{console.log("stop")}));
-//# sourceMappingURL=01-color-switcher.0a2653ba.js.map
