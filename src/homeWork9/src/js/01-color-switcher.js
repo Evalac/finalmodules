a41b184e9a19124c94db2f79295cc9091600c7a4
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-23 16:33:32 +0300

    1111

--- src/homeWork9/src/js/01-color-switcher.js
+++ src/homeWork9/src/js/01-color-switcher.js
@@ -2,11 +2,39 @@ const refs = {
   startBtn: document.querySelector('button[data-start]'),
   stopBtn: document.querySelector('button[data-stop]'),
 };
+let setIntervalId = null;
 
-refs.startBtn.addEventListener('click', () => {
-  console.log('satrt');
-});
+class ChangeColor {
+  constructor() {
+    this.IsActive = false;
+    this.inervalID = null;
+  }
 
-refs.stopBtn.addEventListener('click', () => {
-  console.log('stop');
-});
+  start() {
+    if (this.IsActive) {
+      return;
+    }
+    this.inervalId = setInterval(() => {
+      getRandomHexColor();
+      const color = getRandomHexColor();
+      document.body.style.backgroundColor = color;
+    }, 1000);
+    this.IsActive = true;
+  }
+  stop() {
+    clearInterval(this.inervalId);
+    this.IsActive = false;
+  }
+}
+
+const change = new ChangeColor({});
+console.log(change);
+
+refs.startBtn.addEventListener('click', change.start.bind(change));
+refs.stopBtn.addEventListener('click', change.stop.bind(change));
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
