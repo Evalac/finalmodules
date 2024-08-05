4ab56e538a757afd5446631f37551877ddf17398
Evalac <maslenokprograming@gmail.com>
2024-08-05 21:26:39 +0300

    111

--- src/homeWork9/src/js/02-timer.js
+++ src/homeWork9/src/js/02-timer.js
@@ -12,6 +12,7 @@ const value = flatpickr(refs.inputEl, {});
 refs.buttonEl.addEventListener('click', () => {
   console.log(value.selectedDates);
   console.log(value.now.getTime());
+
   console.log(value);
 });
 
@@ -27,13 +28,15 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
