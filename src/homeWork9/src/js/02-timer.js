b2e5560b3bec12503c8f624ad695ef929d5fc374
Evalac <maslenokprograming@gmail.com>
2024-08-06 15:34:31 +0300

    the timer with date selection is working => class

--- src/homeWork9/src/js/02-timer.js
+++ src/homeWork9/src/js/02-timer.js
@@ -28,38 +28,39 @@ class SaleTimer {
     const selectedDate = value.selectedDates[0].getTime();
     console.log(selectedDate);
 
-    const inervalID = setInterval(() => {
+    this.inervalID = setInterval(() => {
       const startTime = Date.now();
-      console.log(convertMs(selectedDate - startTime));
+      console.log(this.convertMs(selectedDate - startTime));
     }, 1000);
+  }
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    // Remaining seconds
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
+    return { days, hours, minutes, seconds };
   }
 }
 
 const timer = new SaleTimer();
 
 refs.buttonEl.addEventListener('click', timer.timerOn.bind(timer));
-
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
-
-  return { days, hours, minutes, seconds };
-}
