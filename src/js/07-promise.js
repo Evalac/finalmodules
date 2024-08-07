a621d4ec79068b9092ded5a6a5f51dbf87ff85f9
Evalac <maslenokprograming@gmail.com>
2024-08-07 20:24:03 +0300

    promise

--- src/js/07-promise.js
+++ src/js/07-promise.js
@@ -1,1 +1,11 @@
-console.log('promise');
+const makeGreeting = guestName => {
+  if (guestName === '' || guestName === undefined) {
+    return Promise.reject('Guest name must not be empty');
+  }
+
+  return Promise.resolve(`Welcome ${guestName}`);
+};
+
+makeGreeting('Mango')
+  .then(greeting => console.log(greeting))
+  .catch(error => console.error(error));
