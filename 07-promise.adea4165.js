fc4e8a48e87a1539d42115e8784e763f92b44b9d
Evalac <102866941+Evalac@users.noreply.github.com>
2024-08-08 17:51:12 +0000

    Deploying to gh-pages from @ Evalac/finalmodules@96d7915c77ed34fbb9781c5795a822d58b1d795e 🚀

--- 07-promise.adea4165.js
+++
@@ -1,2 +0,0 @@
-var e;(e="Mango",""===e||void 0===e?Promise.reject("Guest name must not be empty"):Promise.resolve(`Welcome ${e}`)).then((e=>console.log(e))).catch((e=>console.error(e)));
-//# sourceMappingURL=07-promise.adea4165.js.map
