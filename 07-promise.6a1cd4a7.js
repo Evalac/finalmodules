0a446117f9096816fddc016abac8b5185437ad23
Evalac <102866941+Evalac@users.noreply.github.com>
2024-08-10 09:57:40 +0000

    Deploying to gh-pages from @ Evalac/finalmodules@2cfd4aee0bed02dabc48b4693d5a2c0750ce5cab 🚀

--- 07-promise.6a1cd4a7.js
+++
@@ -1,2 +0,0 @@
-new Promise(((o,e)=>{const s=Math.random()>.5;setTimeout((()=>{s&&o("promise виконан успішно"),e("Promise виконан з помилкою")}),1e3)})).then((o=>{console.log(`${o}`)}),(o=>{console.log(`${o}`)}));
-//# sourceMappingURL=07-promise.6a1cd4a7.js.map
--- 07-promise.e436841c.js
+++
@@ -1,2 +0,0 @@
-new Promise((function(o,n){var c=Math.random()>.5;setTimeout((function(){c&&o("promise виконан успішно"),n("Promise виконан з помилкою")}),1e3)})).then((function(o){console.log("".concat(o))}),(function(o){console.log("".concat(o))}));
-//# sourceMappingURL=07-promise.e436841c.js.map
