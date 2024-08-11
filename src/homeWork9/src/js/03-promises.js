fab02091cb78aeb6ac78d33480f8cfadd004045b
Evalac <maslenokprograming@gmail.com>
2024-08-11 12:19:58 +0300

    1111

--- src/homeWork9/src/js/03-promises.js
+++ src/homeWork9/src/js/03-promises.js
@@ -1,8 +1,34 @@
+const refs = {
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+  btnSubmit: document.querySelector('button[type="submit"]'),
+};
+
+console.log(refs.btnSubmit);
+
+refs.btnSubmit.addEventListener('click', () => {
+  console.log('click');
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('проміс виконано');
+      }
+      reject('помилка');
+    }, delay);
+  });
 }
+
+createPromise(1, 2000)
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// трекба зробити щоб в параметри записувались значення з інпутів, скоріш за все на них треба відслідковуння родії зробити, може зробити відслідковування подій за допомогою делегування
