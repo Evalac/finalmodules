d3647ed4a97a43abe42c82ea13fa2c9d16f7d94b
Evalac <maslenokprograming@gmail.com>
2024-08-11 09:37:32 +0300

    1111

--- src/js/08-racetrack.js
+++ src/js/08-racetrack.js
@@ -4,24 +4,34 @@ const refs = {
   raceBtn: document.querySelector('.js-race-btn'),
   winner: document.querySelector('.winner'),
   progress: document.querySelector('.progress'),
-  resultTable: document.querySelector('.js-results-table > tbody'),
+  resultTable: document.querySelector('.js-results-table'),
 };
+let raceCounter = 0;
 
 refs.raceBtn.addEventListener('click', () => {
   const promises = horses.map(horse => run(horse));
+  raceCounter += 1;
+
   updateWinnerField();
   updateProgressField(`Заїзд почався ставки не приймаються`);
-  //Promise.race();  цей метод промісу повертає проміс який виконався швидше за інших
-  Promise.race(promises).then(({ horse, time }) => {
-    updateWinnerField(`Переміг ${horse}, фінішував за ${time} часу`);
-    updateResultsTable({ horse, time });
-  });
+  determineWinner(promises);
+  waitForAllHorses(promises);
+});
 
+function waitForAllHorses(promises) {
   //Promise.all() повертає масив всіх виконаних промісів
   Promise.all(promises).then(x => {
     updateProgressField(`Заїзд закінчен, приймаємо ставки`);
   });
-});
+}
+
+function determineWinner(promises) {
+  //Promise.race();  цей метод промісу повертає проміс який виконався швидше за інших
+  Promise.race(promises).then(({ horse, time }) => {
+    updateWinnerField(`Переміг ${horse}, фінішував за ${time} часу`);
+    updateResultsTable({ horse, time, raceCounter });
+  });
+}
 
 function updateWinnerField(message) {
   refs.winner.textContent = message;
@@ -31,10 +41,10 @@ function updateProgressField(message) {
   refs.progress.textContent = message;
 }
 
-function updateResultsTable({ horse, time }) {
-  const tr = `<tr><td>0</td><td>${horse}</td><td>${time}</td></tr>`;
+function updateResultsTable({ horse, time, raceCounter }) {
+  const tr = `<tr><td>${raceCounter}</td><td>${horse}</td><td>${time}</td></tr>`;
 
-  refs.resultTable.innerHTML('beforeend', tr); // помилка із за цього(треба розібратись чтому не иалюється таблиця ((закінчив на 1:52)))
+  refs.resultTable.insertAdjacentHTML('beforeend', tr); // помилка із за цього(треба розібратись чтому не иалюється таблиця ((закінчив на 1:52)))
 }
 
 function run(horse) {
