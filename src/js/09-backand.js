e4eb9c96da05fd16ff43649484123f738c8dc3a3
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-08-16 18:59:37 +0300

    fetch

---
+++ src/js/09-backand.js
@@ -0,0 +1,12 @@
+// import pokemonCard from '';
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then(response => {
+    return response.json();
+  })
+  .then(pokemon => {
+    console.log(pokemon);
+  })
+  .catch(error => {
+    console.log(error);
+  });
