f486a42e410ed8508ee391e0d98eff696a32b6f5
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-08-17 18:39:39 +0300

    ready cart poke

--- src/js/09-backand.js
+++ src/js/09-backand.js
@@ -1,4 +1,4 @@
-// import pokemonCard from '';
+const cartEl = document.querySelector('.pokecart');
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(response => {
@@ -6,7 +6,31 @@ fetch('https://pokeapi.co/api/v2/pokemon/1')
   })
   .then(pokemon => {
     console.log(pokemon);
+    createMarkup(pokemon);
   })
   .catch(error => {
     console.log(error);
   });
+
+function createMarkup(pokemon) {
+  const markup = `<div class="card">
+      <div class="card-img-top">
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+      </div>
+      <div class="card-body">
+        <h2 class="card-title">${pokemon.name}</h2>
+        <p class="card-text">Вага: ${pokemon.weight}</p>
+        <p class="card-text">Ріст: ${pokemon.height}</p>
+        <p class="card-text"><b>Вміння</b></p>
+        <ul class="clas-list-group">
+          ${pokemon.abilities
+            .map(
+              ability =>
+                `<li class="clas-list-item">${ability.ability.name}</li>`
+            )
+            .join('')}
+        </ul>
+      </div>
+    </div>`;
+  cartEl.insertAdjacentHTML('beforeend', markup);
+}
