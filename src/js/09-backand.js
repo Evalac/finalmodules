5e3d84a2b82d0762376d7f39bd263ffaa6f22431
Evalac <maslenokprograming@gmail.com>
2024-08-20 12:14:28 +0300

    1111

--- src/js/09-backand.js
+++ src/js/09-backand.js
@@ -1,12 +1,50 @@
-// import pokemonCard from '';
+import { fetchPokemonById } from './helpers/api-service';
+import { refs } from './helpers/refs';
 
-fetch('https://pokeapi.co/api/v2/pokemon/1')
-  .then(response => {
-    return response.json();
-  })
-  .then(pokemon => {
-    console.log(pokemon);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  const searchPoke = form.elements.query.value;
+
+  fetchPokemonById(searchPoke)
+    .then(pokemon => createMarkup(pokemon))
+    .catch(error => onError(error))
+    .finally(() => {
+      form.reset();
+    });
+}
+
+function createMarkup(pokemon) {
+  const markup = `<div class="card">
+      <div class="card-img-top">
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+      </div>
+      <div class="card-body">
+        <h2 class="card-title">${pokemon.name}</h2>
+        <p class="card-text">Вага: ${pokemon.weight}</p>
+        <p class="card-text">Ріст: ${pokemon.height}</p>
+        <p class="card-text"><b>Вміння</b></p>
+        <ul class="clas-list-group">
+          ${pokemon.abilities
+            .map(ability => (
+              <li class="clas-list-item">${ability.ability.name}</li>
+            ))
+            .join('')}
+        </ul>
+      </div>
+    </div>`;
+  refs.cartEl.innerHTML = markup;
+}
+
+function onError(error) {
+  console.dir(error);
+  alert('Упс щось пішло не так');
+}
+
+// знизу друга вправа по фетчу
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+  .then(responce => responce.json())
+  .then(poke => console.log(poke));
