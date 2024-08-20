b5dd16e92ccdd850ea2f0251d2f35bf54639b685
Evalac <102866941+Evalac@users.noreply.github.com>
2024-08-20 08:32:58 +0000

    Deploying to gh-pages from @ Evalac/finalmodules@4d757cc2accf00f0ec1f5fa3ed4b1dacd86aced5 🚀

---
+++ 09-backand.6fb8ab36.js
@@ -0,0 +1,2 @@
+fetch("https://pokeapi.co/api/v2/pokemon/1").then((function(o){return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.log(o)}));
+//# sourceMappingURL=09-backand.6fb8ab36.js.map
