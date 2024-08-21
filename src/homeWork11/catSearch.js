2b3f15d5dc63660978e09d697629634763ef68fe
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-08-21 21:29:56 +0300

    111

--- src/homeWork11/catSearch.js
+++ src/homeWork11/catSearch.js
@@ -3,12 +3,18 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
 
+const refs = {
+  btnListEl: document.querySelector('.breed-select'),
+};
+
+console.log(refs.btnListEl);
+
 const API_KEY_CAT =
   'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
 
 fetchCats().then(r =>
   r.map(({ id, name }) => {
-    return console.log(id, name);
+    // return console.log(id, name);
   })
 );
 function fetchCats(params) {
