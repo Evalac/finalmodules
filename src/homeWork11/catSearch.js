d403f4fd5572fdcb4b63b93cfb10673b6b0603b7
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-08-22 15:29:44 +0300

    1111

--- src/homeWork11/catSearch.js
+++ src/homeWork11/catSearch.js
@@ -1,23 +1,53 @@
 import axios from 'axios';
+// import { Carousel } from 'bootstrap.native';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
 
 const refs = {
   btnListEl: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
 };
 
-console.log(refs.btnListEl);
+function ShowLoader(params) {
+  refs.loader.classList.remove('hidden');
+  refs.loader.classList.add('visible');
+}
+function HideLoader(params) {
+  refs.loader.classList.remove('visible');
+  refs.loader.classList.add('hidden');
+}
+function ShowError(params) {
+  refs.error.classList.remove('hidden');
+  refs.error.classList.add('visible');
+}
+function HideError(params) {
+  refs.error.classList.remove('visible');
+  refs.error.classList.add('hidden');
+}
 
 const API_KEY_CAT =
   'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
 
-fetchCats().then(r =>
-  r.map(({ id, name }) => {
-    // return console.log(id, name);
+fetchCats()
+  .then(cats => {
+    cats.map(({ id, name }) => {
+      const option = document.createElement('option');
+      option.value = id;
+      option.text = name;
+      refs.btnListEl.add(option);
+      HideLoader();
+    });
   })
-);
+  .catch(error => {
+    HideLoader();
+    ShowError(error);
+  });
+
 function fetchCats(params) {
+  ShowLoader();
+  HideError();
   return fetch(`https://api.thecatapi.com/v1/breeds`).then(responce =>
     responce.json()
   );
