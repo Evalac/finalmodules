625f4b966670ae7754217f25cf19b94629693d07
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-08-22 18:29:34 +0300

    1111

--- src/homeWork11/catSearch.js
+++ src/homeWork11/catSearch.js
@@ -3,12 +3,53 @@ import axios from 'axios';
 
 // axios.defaults.headers.common['x-api-key'] =
 //   'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
+const API_KEY_CAT =
+  'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
 
 const refs = {
   btnListEl: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
+  infoCart: document.querySelector('.cat-info'),
 };
+
+refs.btnListEl.addEventListener('change', evt => {
+  const catId = evt.target.value;
+  fetchCatByID(catId);
+});
+
+fetchCats()
+  .then(cats => {
+    cats.map(({ id, name }) => {
+      const option = document.createElement('option');
+      option.value = id;
+      option.text = name;
+      refs.btnListEl.add(option);
+      HideLoader();
+    });
+  })
+  .catch(error => {
+    HideLoader();
+    ShowError(error);
+  });
+
+function fetchCats() {
+  ShowLoader();
+  HideError();
+  return fetch(`https://api.thecatapi.com/v1/breeds`).then(responce =>
+    responce.json()
+  );
+}
+
+function fetchCatByID(catID) {
+  return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${catID}`)
+    .then(responce => responce.json())
+    .then(catInfo => {
+      catInfo.map(({ url }) => {
+        return (refs.infoCart.innerHTML = `<img src="${url}" alt="" width="300">`);
+      });
+    });
+}
 
 function ShowLoader(params) {
   refs.loader.classList.remove('hidden');
@@ -31,30 +72,4 @@ function ShowError(params) {
 function HideError(params) {
   refs.error.classList.remove('visible');
   refs.error.classList.add('hidden');
-}
-
-const API_KEY_CAT =
-  'live_QDlgVqCBmKpnF37DbgTDS1mKTXHBpluD6Y5yKuT2Acuy938l2FL4U8W9pOKwPfJ0';
-
-fetchCats()
-  .then(cats => {
-    cats.map(({ id, name }) => {
-      const option = document.createElement('option');
-      option.value = id;
-      option.text = name;
-      refs.btnListEl.add(option);
-      HideLoader();
-    });
-  })
-  .catch(error => {
-    HideLoader();
-    ShowError(error);
-  });
-
-function fetchCats(params) {
-  ShowLoader();
-  HideError();
-  return fetch(`https://api.thecatapi.com/v1/breeds`).then(responce =>
-    responce.json()
-  );
 }
