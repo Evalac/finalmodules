71695a1232db2d816a9c1cf5bc37d6be927403af
Evalac <102866941+Evalac@users.noreply.github.com>
2024-08-26 14:20:42 +0000

    Deploying to gh-pages from @ Evalac/finalmodules@364f8024bbf5dfc03169b3ab594f21fe138658f6 🚀

--- 09-backand.0f6cba01.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/").then((function(n){return n.json()})).then((function(n){return console.log(n)}));
-//# sourceMappingURL=09-backand.0f6cba01.js.map
--- 09-backand.c47ea0d0.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/").then((o=>o.json())).then((o=>console.log(o)));
-//# sourceMappingURL=09-backand.c47ea0d0.js.map
