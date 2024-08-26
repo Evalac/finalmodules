843f8131c6a808177decb29031af7c9d411647e7
Evalac <102866941+Evalac@users.noreply.github.com>
2024-08-26 17:30:23 +0000

    Deploying to gh-pages from @ Evalac/finalmodules@76b9d7e0047a9fb84da06493cde05e9f316855c1 🚀

--- 10-newsApi.cd764845.js
+++
@@ -1,2 +0,0 @@
-({searchForm:document.querySelector(".search-form")}).searchForm.addEventListener("submit",(function(e){e.preventDefault()}));fetch("https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1,",{headers:{Authorization:"59ea23f33abe45d5ae23b6a2a5f58a36"}}).then((e=>e.json())).then(console.log);
-//# sourceMappingURL=10-newsApi.cd764845.js.map
