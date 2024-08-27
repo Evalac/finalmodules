7549153d4805117043d42c97a781f3dce327152b
Evalac <maslenokprograming@gmail.com>
2024-08-27 13:34:27 +0300

    1111

--- src/js/10-newsApi.js
+++ src/js/10-newsApi.js
@@ -1,3 +1,5 @@
+import { NewsApiService } from './newsApiHelpers/news-service';
+
 const refs = {
   searchForm: document.querySelector('.search-form'),
   loadMoreBtn: document.querySelector('.load-more-button'),
@@ -6,40 +8,9 @@ const refs = {
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-class NewsApiService {
-  constructor() {
-    this.searchForm = '';
-    this.page = 1;
-  }
-
-  fetchArticles() {
-    console.log(`before`, this);
-    const options = {
-      headers: {
-        Authorization: `59ea23f33abe45d5ae23b6a2a5f58a36`,
-      },
-    };
-
-    const url = `https://newsapi.org/v2/everything?q=${this.searchForm}&language=en&pageSize=5&page=${this.page},`;
-
-    fetch(url, options)
-      .then(responce => responce.json())
-      .then(data => {
-        this.page += 1;
-        console.log(`after`, this);
-      });
-  }
-
-  get query() {
-    return this.searchForm;
-  }
-
-  set query(NewQuery) {
-    this.searchForm = NewQuery;
-  }
-}
-
 const newsApiServise = new NewsApiService();
+
+console.log(newsApiServise);
 
 function onSearch(e) {
   e.preventDefault();
---
+++ src/js/newsApiHelpers/news-service.js
@@ -0,0 +1,34 @@
+class NewsApiService {
+  constructor() {
+    this.searchForm = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    console.log(`before`, this);
+    const options = {
+      headers: {
+        Authorization: `59ea23f33abe45d5ae23b6a2a5f58a36`,
+      },
+    };
+
+    const url = `https://newsapi.org/v2/everything?q=${this.searchForm}&language=en&pageSize=5&page=${this.page},`;
+
+    fetch(url, options)
+      .then(responce => responce.json())
+      .then(data => {
+        this.page += 1;
+        console.log(`after`, this);
+      });
+  }
+
+  get query() {
+    return this.searchForm;
+  }
+
+  set query(NewQuery) {
+    this.searchForm = NewQuery;
+  }
+}
+
+export { NewsApiService };
