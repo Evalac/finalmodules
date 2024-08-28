de0f9e07d866aa6d606609904ad87cc1b83a966f
Evalac <maslenokprograming@gmail.com>
2024-08-28 17:45:08 +0300

    1111

--- src/js/11-crud.js
+++ src/js/11-crud.js
@@ -1,4 +1,7 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
+// Change this number to fetch different post
+const postId = 2;
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
   .then(response => response.json())
-  .then(posts => console.log(posts))
+  .then(post => console.log(post))
   .catch(error => console.log(error));
